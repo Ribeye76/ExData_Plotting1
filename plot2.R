4ed0594c660e8f6f950179e83e19bad5eaab33d6
Jose Maria Rivas Carvalho <jose.rivas.mx@gmail.com>
2016-06-25 01:14:40 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,7 +2,6 @@ file <- "household_power_consumption.txt"
 hpc <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(hpc$Date, hpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-gAP <- as.numeric(hpc$Global_active_power)
 png("plot2.png", width=480, height=480)
-plot(datetime, gAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(datetime, as.numeric(hpc$Global_active_power), type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
