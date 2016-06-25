3341aaeb914b6ac814c6067bca6b879c5a65a7b5
Jose Maria Rivas Carvalho <jose.rivas.mx@gmail.com>
2016-06-25 00:53:17 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -20,4 +20,10 @@ plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reac
 dev.off()
 
 ##-------------
-
+file <- "household_power_consumption.txt"
+hpc <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
+gAP <- as.numeric(hpc$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(gAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
