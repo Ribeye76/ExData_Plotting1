64a044b92139159115915283e2389f82bcaec27f
Jose Maria Rivas Carvalho <jose.rivas.mx@gmail.com>
2016-06-25 00:35:00 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,3 @@
-# read.file
 file <- "household_power_consumption.txt"
 hpc <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
