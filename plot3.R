f8bc9bac5a9617e50e17d167555522e6413928ad
Jose Maria Rivas Carvalho <jose.rivas.mx@gmail.com>
2016-06-25 00:50:42 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,17 +1,14 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-#str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
-
+file <- "household_power_consumption.txt"
+hpc <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(hpc$Date, hpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+gAP <- as.numeric(hpc$Global_active_power)
+sM1 <- as.numeric(hpc$Sub_metering_1)
+sM2 <- as.numeric(hpc$Sub_metering_2)
+sM3 <- as.numeric(hpc$Sub_metering_3)
 png("plot3.png", width=480, height=480)
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
+plot(datetime, sM1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, sM2, type="l", col="red")
+lines(datetime, sM3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
